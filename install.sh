6d826de912706b5c48eb816f75f5ca7e1283c0b5
R37r0-Gh057 <40432896+R37r0-Gh057@users.noreply.github.com>
2020-06-22 13:57:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-msf_6 () {
-	pkg install -y autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ruby ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner resolv-conf pkg-config
-
+msf_6 ()
+        apt update
+        apt update --fix-missing
+	apt install perl libllvm protobuf c-ares libltdl glib libandroid-shmem libicu libyaml autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ruby ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner resolv-conf pkg-config -y
 }
 
 if [ "$1" == 'msf' ];
 then
 	msf_6
 else
-	pkg install -y neofetch python
+	apt install neofetch python -y
 	python3 -m pip install requests
 fi
