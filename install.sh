b78eeba18253862c4875c96a089312a1a6cf2d28
Ethical-Hacking-Zone <55155544+Ethical-Hacking-Zone@users.noreply.github.com>
2020-06-23 23:34:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ msf_6 ()
         apt update
         apt update --fix-missing
 	apt install perl libllvm protobuf c-ares libltdl glib libandroid-shmem libicu libyaml autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ruby ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner resolv-conf pkg-config -y
-}
+
 
 if [ "$1" == 'msf' ];
 then
